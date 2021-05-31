fcca84b94ffd3064179e77d57ddfa8a0059ec8b8
woorisx <83794443+woorisx@users.noreply.github.com>
2021-05-31 17:25:32 +0900

    new updates

---
+++ PHP/dev-web-02.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DevWEB02</title>
+</head>
+<body>
+ 
+<?php
+
+    $name = $_POST["name"];
+    echo $name."님 반갑습니다~~<br>";
+?>
+
+</body>
+</html>
